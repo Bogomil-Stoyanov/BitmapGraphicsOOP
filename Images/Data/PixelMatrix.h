f0c28a75374451816eeef90d87fdadd25ea95bcb
Bogomil Stoyanov <bogi2208@gmail.com>
2023-04-15 15:31:40 +0300

    Code cleanup

--- Images/Data/PixelMatrix.h
+++ Images/Data/PixelMatrix.h
@@ -8,47 +8,63 @@
 #include <cstdint>
 #include <functional>
 
-template <typename T>
+template<typename T>
 class PixelMatrix {
 public:
-    PixelMatrix(const int rows = 0, const int cols = 0);
-    PixelMatrix(const PixelMatrix<T>& other);
+    PixelMatrix(int rows = 0, int cols = 0);
+
     ~PixelMatrix();
 
     int getRows() const;
+
     int getCols() const;
 
-    void readFromFile(std::ifstream& file);
-    void writeToFile(std::ofstream& file) const;
+    void readFromFile(std::ifstream &file);
 
-    void resize(const int rows, const int cols);
+    void writeToFile(std::ofstream &file) const;
 
-    void rotateRight();
-    void rotateLeft();
+    void rotate(bool isRight);
 
-    T& getElementAt(const int row, const int col);
-    const T& getElementAt(const int row, const int col) const;
+    T &getElementAt(int row, int col);
+
     void setElementAt(int row, int col, T elem);
 
-    PixelMatrix<T>& operator=(const PixelMatrix<T>& other);
+    PixelMatrix<T> &operator=(const PixelMatrix<T> &other);
 
-//private:
-    T** data;
+    T &getElement(int row, int col) const;
+
+    void fillPixelMatrixWithZeroes(int rows, int cols);
+
+    void resize(int rows, int cols);
+
+
+private:
+    T **data;
     int countRows;
     int countColumns;
 
     void deleteData();
-    void copyPixelMatrix(const PixelMatrix<T>& other);
-    void copy(const PixelMatrix<T>& other);
 
-    void fillPixelMatrix(const int rows, const int cols, std::function<void(T&, int, int)> pixelLogicFiller);
+    void copy(const PixelMatrix<T> &other);
 
-    void fillPixelMatrixWithZeroes(const int rows, const int cols);
+
+    void fillPixelMatrix(int rows, int cols, std::function<void(T &, int, int)> pixelLogicFiller);
+
     void transpose();
-    void reverseRows();
 
-    T& getElement(const int row, const int col) const;
+    void reverseRows();
 };
+
+template<typename T>
+void PixelMatrix<T>::rotate(bool isRight) {
+    if (isRight) {
+        reverseRows();
+        transpose();
+    } else {
+        transpose();
+        reverseRows();
+    }
+}
 
 template<typename T>
 void PixelMatrix<T>::setElementAt(int row, int col, T elem) {
@@ -60,11 +76,6 @@ template<typename T>
 inline PixelMatrix<T>::PixelMatrix(const int rows, const int cols)
         : data(nullptr), countRows(rows), countColumns(cols) {
     fillPixelMatrixWithZeroes(rows, cols);
-}
-
-template<typename T>
-inline PixelMatrix<T>::PixelMatrix(const PixelMatrix<T>& other) {
-    copy(other);
 }
 
 template<typename T>
@@ -83,16 +94,16 @@ inline int PixelMatrix<T>::getCols() const {
 }
 
 template<typename T>
-inline void PixelMatrix<T>::readFromFile(std::ifstream& file) {
-    for (int row = 0; row < countRows; ++row){
-        for (int col = 0; col < countColumns; ++col){
+inline void PixelMatrix<T>::readFromFile(std::ifstream &file) {
+    for (int row = 0; row < countRows; ++row) {
+        for (int col = 0; col < countColumns; ++col) {
             file >> data[row][col];
         }
     }
 }
 
 template<typename T>
-inline void PixelMatrix<T>::writeToFile(std::ofstream& file) const {
+inline void PixelMatrix<T>::writeToFile(std::ofstream &file) const {
     for (int row = 0; row < countRows; ++row) {
         for (int col = 0; col < countColumns; ++col)
             file << data[row][col] << (col == countColumns - 1 ? '\n' : ' ');
@@ -100,41 +111,13 @@ inline void PixelMatrix<T>::writeToFile(std::ofstream& file) const {
 }
 
 template<typename T>
-inline void PixelMatrix<T>::resize(const int rows, const int cols) {
-    fillPixelMatrix(rows, cols, [=](T& elem, int row, int col) {
-        if (row < countRows && col < countColumns)
-            elem = data[row][col];
-        else
-            elem = 0;
-    });
-}
-
-template<typename T>
-inline void PixelMatrix<T>::rotateRight() {
-    reverseRows();
-    transpose();
-}
-
-template<typename T>
-inline void PixelMatrix<T>::rotateLeft() {
-    transpose();
-    reverseRows();
-}
-
-template<typename T>
-inline T& PixelMatrix<T>::getElementAt(const int row, const int col) {
+inline T &PixelMatrix<T>::getElementAt(const int row, const int col) {
     return getElement(row, col);
 }
 
 template<typename T>
-inline const T& PixelMatrix<T>::getElementAt(const int row, const int col) const {
-    return getElement(row, col);
-}
-
-template<typename T>
-inline PixelMatrix<T>& PixelMatrix<T>::operator=(const PixelMatrix<T>& other) {
+inline PixelMatrix<T> &PixelMatrix<T>::operator=(const PixelMatrix<T> &other) {
     if (this != &other) copy(other);
-
     return *this;
 }
 
@@ -144,18 +127,17 @@ inline void PixelMatrix<T>::deleteData() {
         delete[] data[row];
         data[row] = nullptr;
     }
-
     delete[] data;
-
     data = nullptr;
 }
 
 template<typename T>
-inline void PixelMatrix<T>::copyPixelMatrix(const PixelMatrix<T>& other) {
+inline void PixelMatrix<T>::copy(const PixelMatrix<T> &other) {
+    deleteData();
     countRows = other.countRows;
     countColumns = other.countColumns;
 
-    data = new T * [countRows];
+    data = new T *[countRows];
 
     for (int row = 0; row < countRows; ++row) {
         data[row] = new T[countColumns];
@@ -166,46 +148,50 @@ inline void PixelMatrix<T>::copyPixelMatrix(const PixelMatrix<T>& other) {
 }
 
 template<typename T>
-inline void PixelMatrix<T>::copy(const PixelMatrix<T>& other) {
-    deleteData();
-    copyPixelMatrix(other);
-}
-
-template<typename T>
-inline void PixelMatrix<T>::fillPixelMatrix(const int rows, const int cols, std::function<void(T&, int, int)> pixelLogicFiller) {
-    T** blockOfMem = new T * [rows];
+inline void
+PixelMatrix<T>::fillPixelMatrix(const int rows, const int cols, std::function<void(T &, int, int)> pixelLogicFiller) {
+    T **newData = new T *[rows];
 
     for (int row = 0; row < rows; ++row) {
-        blockOfMem[row] = new T[cols];
-
-        for (int col = 0; col < cols; ++col)
-            pixelLogicFiller(blockOfMem[row][col], row, col);
+        newData[row] = new T[cols];
+        for (int col = 0; col < cols; ++col) {
+            pixelLogicFiller(newData[row][col], row, col);
+        }
     }
 
     deleteData();
-    data = blockOfMem;
+    data = newData;
     countRows = rows;
     countColumns = cols;
 }
 
+
 template<typename T>
 inline void PixelMatrix<T>::fillPixelMatrixWithZeroes(const int rows, const int cols) {
-    fillPixelMatrix(rows, cols, [](T& elem, int, int) { elem = 0; });
+    fillPixelMatrix(rows, cols, [](T &elem, int, int) { elem = 0; });
 }
 
 template<typename T>
 inline void PixelMatrix<T>::transpose() {
-    fillPixelMatrix(countColumns, countRows, [=](T& elem, int row, int col) { elem = data[col][row]; });
+    fillPixelMatrix(countColumns, countRows, [=](T &elem, int row, int col) { elem = data[col][row]; });
 }
 
 template<typename T>
 inline void PixelMatrix<T>::reverseRows() {
-    fillPixelMatrix(countRows, countColumns, [=](T& elem, int row, int col) { elem = data[countRows - row - 1][col]; });
+    fillPixelMatrix(countRows, countColumns, [=](T &elem, int row, int col) { elem = data[countRows - row - 1][col]; });
 }
 
 template<typename T>
-inline T& PixelMatrix<T>::getElement(const int row, const int col) const {
-   return data[row][col];
+inline T &PixelMatrix<T>::getElement(const int row, const int col) const {
+    return data[row][col];
+}
+
+template<typename T>
+inline void PixelMatrix<T>::resize(const int rows, const int cols) {
+    fillPixelMatrix(rows, cols, [=](T &elem, int row, int col) {
+        if (row < countRows && col < countColumns) { elem = data[row][col]; }
+        else { elem = 0; }
+    });
 }
 
 
